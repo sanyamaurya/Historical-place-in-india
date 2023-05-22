2bdaa24d1913f0411367ab48bc75532825147436
sanyamaurya <132988357+sanyamaurya@users.noreply.github.com>
2023-05-22 14:24:07 +0530

    Add files via upload

---
+++ problem 6 .py
@@ -0,0 +1,5 @@
+p = float(input("The Principle amount is: "))
+T = int(input("The Time Period in year is: "))
+R = float(input("Rate of Interest: "))
+Simple_Interest = (P*T*R)/100
+print("The simple interest is ", Simple_Interest)
