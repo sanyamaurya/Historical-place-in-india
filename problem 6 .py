794bf81849a747a72bc2f8e0508e69762a85bffb
sanyamaurya <132988357+sanyamaurya@users.noreply.github.com>
2023-05-22 14:39:25 +0530

    Delete problem 6 .py

--- problem 6 .py
+++
@@ -1,5 +0,0 @@
-p = float(input("The Principle amount is: "))
-T = int(input("The Time Period in year is: "))
-R = float(input("Rate of Interest: "))
-Simple_Interest = (P*T*R)/100
-print("The simple interest is ", Simple_Interest)
